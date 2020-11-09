7a5ecc9a46c154454c5bc4e999cd9e7642f8b165
Patipatkul <sc.patipatk@gmail.com>
2020-11-09 09:41:54 +0700

    update frontend.js

--- frontend.js
+++ frontend.js
@@ -3,35 +3,18 @@ const app = express();
 const fs = require("fs");
 const frontEndPort = process.env.FE_PORT || 3303;
 const appIp = "0.0.0.0";
-app.use(express.static(__dirname + "/dist/saveWork"));
 
-app.use("/saveWork", (req, res) => {
+app.use(express.static(path.join(__dirname, './dist/saveWork/')));
 
-  console.log('11111');
+app.use('/', (req, res) => {
+
+  console.log('222222222');
 
   // console.log(path.join(__dirname, './dist/sgl-product-mgmt/index.html'));
-  res.sendFile(path.join(__dirname, "./dist/saveWork/index.html"));
+  res.sendFile(path.join(__dirname, './dist/saveWork/index.html'));
 });
-
-// app.use('/saveWork', (req, res) => {
-//     // console.log(path.join(__dirname, './dist/sgl-product-mgmt/index.html'));
-//     res.sendFile(path.join(__dirname, './dist/saveWork/index.html'));
-//   });
-//use https
-// const https = require('https');
-// const key = fs.readFileSync(__dirname+'/ssl/the-cool-fe-key.pem');
-// const cert = fs.readFileSync(__dirname+'/ssl/the-cool-fe-cert.pem');
-// const options = {
-//     key: key,
-//     cert: cert,
-//     requestCert: false
-// };
-// https.createServer(options, app).listen(frontEndPort, appIp, function(err) {
-//     console.log(`The Cool app listening on port  ${frontEndPort}`);
-// });
 
 //use http
 app.listen(frontEndPort, function () {
-  console.log('222222');
   console.log("The Cool app listening on port " + frontEndPort + "!");
 });
